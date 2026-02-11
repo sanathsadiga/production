0c614ad994af22b382c51b0d9695e188d7806fda
Sanath S Adiga <sanathadigas@gmail.com>
2026-02-11 12:19:50 +0530

    added location wise and VK and OSP vise publications, enhanced dashboard

--- database/migrations/004_seed_machines.sql
+++
@@ -1,3 +0,0 @@
-USE mmcl_production;-- Disable foreign key checks temporarily
-SET FOREIGN_KEY_CHECKS=0;DELETE FROM machines WHERE id > 0;INSERT INTO machines (name, code) VALUES('Machine A', 'MA'),('Machine B', 'MB'),('Machine C', 'MC'),('Machine D', 'MD');-- Re-enable foreign key checks
-SET FOREIGN_KEY_CHECKS=1;
--- database/seeds/004_newsprint_types.sql
+++
@@ -1,9 +0,0 @@
--- Seed: Newsprint Types Master Data
--- These use INSERT IGNORE to safely skip duplicates if re-run
-
-USE mmcl_production;
-
-INSERT IGNORE INTO newsprint_types (name, code) VALUES
-('Ramdas', 'RMD'),
-('Shree Lakshmi Tulasi', 'SLT'),
-('Imported', 'IMP');
